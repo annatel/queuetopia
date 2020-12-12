2e176f5c635057078ab2bbb9815ae37af5a97c0b
Laetitia <l.ohayon@bevaday.com>
2020-12-12 21:13:33 +0200

    add test assertions

--- lib/queuetopia/test/assertions.ex
+++ lib/queuetopia/test/assertions.ex
@@ -1,19 +1,41 @@
 defmodule Queuetopia.Test.Assertions do
   import ExUnit.Assertions
+  require Ecto.Query
   alias Queuetopia.Jobs.Job
 
   @doc """
   Asserts the job has juste been created
   """
 
-  def assert_job_created(queue, %{} = params) do
-    repo = queue.repo()
+  def assert_job_created(queuetopia) when is_atom(queuetopia) do
+    repo = queuetopia.repo()
+
+    job = Job |> Ecto.Query.last() |> repo.one()
+    refute is_nil(job)
+    job
+  end
+
+  def assert_job_created(queuetopia, queue) when is_atom(queuetopia) and is_binary(queue) do
+    repo = queuetopia.repo()
 
     job =
       Job
+      |> Ecto.Query.where([job], job.queue == ^queue)
       |> Ecto.Query.last()
       |> repo.one()
 
-    refute is_nil(job) && assert(^params = job)
+    refute is_nil(job)
+    job
+  end
+
+  def assert_job_created(queuetopia, %{} = params) when is_atom(queuetopia) do
+    job = assert_job_created(queuetopia)
+    assert ^params = job
+  end
+
+  def assert_job_created(queuetopia, queue, %{} = params)
+      when is_atom(queuetopia) and is_binary(queue) do
+    job = assert_job_created(queuetopia, queue)
+    assert ^params = job
   end
 end
