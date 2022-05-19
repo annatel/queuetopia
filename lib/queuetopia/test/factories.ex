39d39ba81d472117e2e18a99be54ac9dcea4c2c7
Laetitia <l.ohayon@bevaday.com>
2022-05-19 12:17:27 +0300

    typo

--- lib/queuetopia/test/factories.ex
+++ lib/queuetopia/test/factories.ex
@@ -1,13 +1,15 @@
 defmodule Queuetopia.Factories do
+  alias Queuetopia.Queue.Job
+
   def build(:job, attrs) do
     %Job{
-      sequence: System.unique_integer([:positive]),,
+      sequence: System.unique_integer([:positive]),
       scope: "scope_#{System.unique_integer([:positive])}",
       queue: "queue_#{System.unique_integer([:positive])}",
       performer: "performer",
       action: "action_#{System.unique_integer([:positive])}",
       params: %{},
-      scheduled_at: utc_now(),
+      scheduled_at: DateTime.utc_now(),
       timeout: 5_000,
       max_backoff: 0,
       max_attempts: 20
